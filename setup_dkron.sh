8a6848d717851131fa05ae8d57b4a1d84517ad3e
lintang-b-s <lintangbirdasaputra23@gmail.com>
2024-05-27 10:42:48 +0700

    change auth docker image

--- setup_dkron.sh
+++ setup_dkron.sh
@@ -1,6 +1,6 @@
 docker exec -it dkron sh /curl/curl.sh
-curl localhost:9911/v1/jobs -XPOST -d @scheduled_metrics_jobs.json
-curl localhost:9911/v1/jobs -XPOST -d @scheduled_terminated_container_jobs.json
+curl  -XPOST localhost:9911/v1/jobs -d @scheduled_metrics_jobs.json
+curl -XPOST localhost:9911/v1/jobs  -d @scheduled_terminated_container_jobs.json
 
 
 
