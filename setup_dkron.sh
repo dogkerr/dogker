49b68042c839b795245c313c7bf48787c8006df3
lintang-b-s <lintangbirdasaputra23@gmail.com>
2024-05-28 04:58:58 +0700

    fix bug ctr lifecyle ketambah sendiri

--- setup_dkron.sh
+++ setup_dkron.sh
@@ -2,7 +2,6 @@ docker exec -it dkron sh /curl/curl.sh
 curl  -XPOST localhost:9911/v1/jobs -d @scheduled_metrics_jobs.json
 curl -XPOST localhost:9911/v1/jobs  -d @scheduled_terminated_container_jobs.json
 
-curl -XPOST localhost:9911/v1/jobs  -d @scheduled_fix_stop_status_container.json
 
 
 
